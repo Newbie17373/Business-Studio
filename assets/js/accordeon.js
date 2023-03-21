4d4177fc24f90ff068327c95dc83ce361c15c846
Bakerman <74716816+Newbie17373@users.noreply.github.com>
2023-03-22 00:34:12 +0600

    Delete assets directory

--- assets/js/accordeon.js
+++
@@ -1,7 +0,0 @@
-const acc = document.getElementsByClassName('accordion')
-
-for(let i = 0; i < acc.length; i++) {
-acc[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-})
-}
